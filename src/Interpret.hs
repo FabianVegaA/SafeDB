7a04a3c2bffb5b89abaf2a49a4adb91b22303604
FABIÁN MATÍAS VEGA ALCOTA <fabian.vegaa@sansano.usm.cl>
2022-11-11 00:58:06 -0300

    feat(interpret): add insert

--- src/Interpret.hs
+++ src/Interpret.hs
@@ -1,56 +1,43 @@
 module Interpret (runDB) where
 
 import Control.Exception (throwIO)
-import Data.Aeson (decode, encode)
+import Data.Aeson (FromJSON, ToJSON, decode, encode)
+import Data.Aeson.Encode.Pretty (encodePretty)
+import qualified Data.ByteString.Lazy.Char8 as B
 import Data.ByteString.Lazy.Internal (packChars, unpackChars)
 import Free (Free (..))
-import Lib (Operation (..), OperationFail (..))
-import Record (Person (..), Record (..), RecordPerson)
+import Lib (DBOperation, Operation (..), OperationFail (..))
+import Record (Record (..))
 import qualified System.IO.Strict as S
 
-runDB :: (Show r) => Free (Operation Int Person) r -> IO ()
-runDB (Free (Init next)) = run next initDB
-runDB (Free (Get key next)) = run next $ getDB key
-runDB (Free (Set key value next)) = run next $ setDB key value
-runDB (Free (Show key next)) = run next $ showDB key
-runDB (Free Done) = return ()
-runDB (Pure r) = throwIO . userError $ show r
-
-run :: Show r => Free (Operation Int Person) r -> IO a -> IO ()
-run next routine = routine >> runDB next
-
-initDB :: IO ()
-initDB = do
-  putStrLn "Starting DB with default connection (test.fiabledb)"
-  writeFile "test.fiabledb" mempty
-
-getDB :: Int -> IO [RecordPerson]
-getDB key = do
-  db <- S.readFile "test.fiabledb"
-  case decode $ packChars db of
-    Just records -> return $ filter (\r -> identifier r == key) records
-    Nothing -> return []
-
-setDB :: Int -> Person -> IO ()
-setDB key value = do
-  db <- S.readFile "test.fiabledb"
-  res <- getDB key
-  addRecord db $ case res of
-    [] -> Record 1 value 1
-    rs ->
-      let next_version = maximum (map identifier rs) + 1
-          id' = identifier $ head rs
-       in Record id' value next_version
+runDB :: (ToJSON a, FromJSON a, Show a, Show b) => DBOperation a b -> IO ()
+runDB (Pure err) = throwOperation err
+runDB (Free a) = case a of
+  Init next -> do
+    putStrLn "Starting DB with default connection (test.fiabledb)"
+    writeFile "test.fiabledb" mempty
+    runDB next
+  Get key next -> do
+    rs <- connectDB
+    case rs of
+      Nothing -> throwOperation DBConnectionError
+      Just records -> do
+        putStr "Records found: "
+        let foundRecords = filter (\r -> identifier r == key) records
+        if null foundRecords
+          then throwOperation KeyNotFound
+          else B.putStrLn . encodePretty $ foundRecords
+        runDB next
+  Insert val next -> do
+    rs <- connectDB
+    let record = Record (length rs + 1) 1 val
+    writeFile "test.fiabledb" . unpackChars . encode $ case rs of
+      Just records -> record : records
+      Nothing -> [record]
+    runDB next
+  Done -> putStrLn "Closing DB connection"
   where
-    addRecord :: String -> RecordPerson -> IO ()
-    addRecord db record = do
-      writeFile "test.fiabledb" . unpackChars . encode $ case decode (packChars db) of
-        Just records -> record : records
-        Nothing -> [record]
+    connectDB = decode . packChars <$> S.readFile "test.fiabledb"
 
-showDB :: Int -> IO ()
-showDB key = do
-  res <- getDB key
-  case res of
-    [] -> throwIO . userError $ show KeyNotFound
-    rs -> putStrLn $ "Records: " ++ show rs
+throwOperation :: (Show a) => a -> IO ()
+throwOperation = throwIO . userError . show
