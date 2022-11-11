1b4b97598ba5bea238a892e234c6bbb79356dd60
FABIÁN MATÍAS VEGA ALCOTA <fabian.vegaa@sansano.usm.cl>
2022-11-11 00:57:00 -0300

    fix(record): remove person data
    Rename version fild rev

--- src/Record.hs
+++ src/Record.hs
@@ -6,27 +6,13 @@ import Data.Aeson (FromJSON (..), ToJSON (..), object, withObject, (.:), (.=))
 
 data Record a = Record
   { identifier :: Int,
-    value :: a,
-    version :: Int
+    rev :: Int,
+    value :: a
   }
   deriving (Show)
-
-data Person = Person
-  { name :: String,
-    age :: Int
-  }
-  deriving (Show)
-
-type RecordPerson = Record Person
 
 instance (FromJSON a) => FromJSON (Record a) where
   parseJSON = withObject "Record" $ \v -> Record <$> v .: "id" <*> v .: "value" <*> v .: "version"
 
-instance FromJSON Person where
-  parseJSON = withObject "Person" $ \v -> Person <$> v .: "name" <*> v .: "age"
-
 instance (ToJSON a) => ToJSON (Record a) where
   toJSON (Record id value version) = object ["id" .= id, "value" .= value, "version" .= version]
-
-instance ToJSON Person where
-  toJSON (Person name age) = object ["name" .= name, "age" .= age]
